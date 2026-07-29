93c379dfa585e81b23a10ab868f424d92fd1fd7e
abdo0070 <abdallammdouh6@gmail.com>
2026-07-29 12:08:46 +0300

    edit in routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from './components/dashboard/dashboard';
+import { Login } from './auth/login/login';
+import { Notfound } from './components/notfound/notfound';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+    { path: 'dashboard', component: Dashboard },
+    {path : "login" , component:Login},
+    {path : "**" ,component:Notfound}
+];
