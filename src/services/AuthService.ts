2695f966761c1c5d2c5fa1feb54e323bf94cf4a3
abdo0070 <abdallammdouh6@gmail.com>
2026-08-01 15:32:56 +0300

    edit in userService&AuthService

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -1,15 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { UserService } from './UserService';
+import { User } from '../app/data/User';
 
 @Injectable({
   providedIn: 'root' // Makes the service available throughout the app
 })
 export class AuthService {
+  constructor(private user : UserService,private http: HttpClient){}
   api_url = 'https://localhost:7097';
   private token : string = '';
-  // Constructor must be public (or default) so Angular DI can instantiate it
-  constructor(private http: HttpClient) {}
-
   public login(email:string,password:string) : string {
     // make request
     this.http.post(`${this.api_url}/auth`, {
@@ -17,11 +17,21 @@ export class AuthService {
       Password: password,
     },{responseType : 'text'}).subscribe(res => {
         this.token = res;
+        // store the token in local storage
+        localStorage.setItem("token",this.token);
+        // load user data
+        this.user.load();
     });
     return this.token;
   }
 
-  public register() {}
+  public register() {
+    
+  }
 
-  public logout() {}
+  public logout() {
+    // set user service to null
+    this.user.curUser.set(null);
+    localStorage.removeItem("token");
+  }
 }
--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,10 +1,16 @@
 import { Injectable, signal } from "@angular/core";
 import { User } from "../app/data/User";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({
     providedIn : 'root'
 })
 export class UserService
 {
+    constructor(http : HttpClient) {}
     curUser = signal<User|undefined|null>(null); 
+    load(){
+        const token : string | null = localStorage.getItem("token");
+        // make req to load user data from the API
+    }
 }
